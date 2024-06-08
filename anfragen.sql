986091ac71f301fb4d7312e2b549cc6c5b7f6ba9
Julian Mosig von Aehrenfeld <gr15iped@studserv.uni-leipzig.de>
2024-06-08 14:49:47 +0200

    write query for the best rated products per type

--- anfragen.sql
+++ anfragen.sql
@@ -11,3 +11,27 @@ SELECT * FROM
 	(SELECT COUNT (dvd_id) num_dvds FROM dvdspec)
 	;
 
+-- Nennen Sie die 5 besten Produkte jedes Typs (Buch, Musik-CD, DVD) sortiert nach dem durchschnittlichem Rating. 
+-- Hinweis: Geben Sie das Ergebnis in einer einzigen Relation mit den Attributen Typ, ProduktNr, Rating aus. 
+-- Wie werden gleiche durchschnittliche Ratings behandelt?
+
+-- Hilfsmethode, gibt alle average ratings für alle items
+SELECT * FROM
+(SELECT asin, AVG(rating) average_rating FROM product_reviews r GROUP BY asin) NATURAL JOIN (SELECT asin, pgroup FROM item)
+ORDER BY pgroup, average_rating DESC
+
+
+-- nimmt aus der hilfsmethode immer die 5 mit dem höchsten rating pro type
+SELECT
+    *
+FROM
+    (SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY pgroup ORDER BY average_rating DESC) AS row_num -- row_number zählt die zeilen
+    FROM
+        (SELECT * FROM
+			(SELECT asin, AVG(rating) average_rating FROM product_reviews r GROUP BY asin) NATURAL JOIN (SELECT asin, pgroup FROM item)
+		)
+)
+WHERE
+    row_num <= 5; -- Nimm nur die 5 ersten Ergebnisse pro Typ, also ersten 5 row_number
