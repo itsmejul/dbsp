2fcf405781372dac8477aad0d44413b9bea88e1c
Julian Mosig von Aehrenfeld <gr15iped@studserv.uni-leipzig.de>
2024-06-08 14:31:13 +0200

    write query for number of each item type

--- anfragen.sql
+++ anfragen.sql
@@ -1,1 +1,13 @@
 SELECT * FROM item
+
+
+
+-- Wieviele Produkte jeden Typs (Buch, Musik-CD, DVD) sind in der Datenbank erfasst? 
+-- Hinweis: Geben Sie das Ergebnis in einer 3-spaltigen Relation aus.
+
+SELECT * FROM 
+	(SELECT COUNT (book_id) num_books FROM bookspec),
+	(SELECT COUNT (cd_id) num_cds FROM musicspec),
+	(SELECT COUNT (dvd_id) num_dvds FROM dvdspec)
+	;
+
