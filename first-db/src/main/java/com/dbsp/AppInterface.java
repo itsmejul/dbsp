76aa22cd28b2612182508f0ce290a69174c04ee6
Julian Mosig von Aehrenfeld <gr15iped@studserv.uni-leipzig.de>
2024-09-22 09:38:51 +0200

    Make Output of GetProductsByCategoryPath prettier

--- first-db/src/main/java/com/dbsp/AppInterface.java
+++ first-db/src/main/java/com/dbsp/AppInterface.java
@@ -9,6 +9,7 @@ import com.dbsp.entity.ProductReviews;
 import com.dbsp.extra.Category;
 
 public interface AppInterface {
+
     void init(); // Initialisierungsmethode
 
     void finish();
@@ -18,20 +19,23 @@ public interface AppInterface {
     List<Item> getProducts(String pattern);
 
     Category getCategoryTree();
+
     List<Item> getProductsByCategoryPath(String categoryPath);
 
-    List<Item> getTopProducts(int k); //Items unter den Top k Ratings
+    List<Item> getTopProducts(int k); // Items unter den Top k Ratings
 
     List<Item> getSimilarCheaperProduct(String asin);
 
-    //unsicher wegen dem Review Ding!
-    void addNewReview(String asin, Integer rating, Integer helpful, String reviewDate, Integer customerId, String summary, String content);
+    // unsicher wegen dem Review Ding!
+    void addNewReview(String asin, Integer rating, Integer helpful, String reviewDate, Integer customerId,
+            String summary, String content);
+
     List<ProductReviews> showReviews(String asin);
 
-    List<Customer>getTrolls(double averageRating);
+    List<Customer> getTrolls(double averageRating);
 
-    List<Price>getOffers(String asin);
-    //extra Methoden
+    List<Price> getOffers(String asin);
+    // extra Methoden
 
     void addShop(String name, String street, int zip);
 
